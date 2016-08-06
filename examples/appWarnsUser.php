81ba323661b2911ac7feb10479c7b261903be6dd
Sapphire Cat <devel@sapphirepaw.org>
2016-08-06 17:54:35 -0400

    Unset the handler before exiting the loop.

--- examples/appWarnsUser.php
+++ examples/appWarnsUser.php
@@ -5,6 +5,7 @@ require(__DIR__.'/common.inc.php');
 foreach (collect_filenames($argv) as $filename) {
     $handle = Signal::receive('app_handle');
     print_r(analyze_log($filename));
+    unset($handle);
 }
 
 
