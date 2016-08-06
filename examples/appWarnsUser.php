022504af5885281bd3b00342edc3eb38669783a4
Sapphire Cat <devel@sapphirepaw.org>
2016-08-06 17:52:23 -0400

    Add example that displays warnings for errors.

---
+++ examples/appWarnsUser.php
@@ -0,0 +1,21 @@
+<?php
+use Sapphirecat\Signal\Signal;
+
+require(__DIR__.'/common.inc.php');
+foreach (collect_filenames($argv) as $filename) {
+    $handle = Signal::receive('app_handle');
+    print_r(analyze_log($filename));
+}
+
+
+function app_handle ($restarts, $signal, $args)
+{
+    if ($signal !== 'error') {
+        return; // don't do anything about non-errors
+    }
+
+    list($message, $data) = $args;
+    Signal::warning("Ignoring an error: $message");
+
+    $restarts->call('SkipEntry');
+}
